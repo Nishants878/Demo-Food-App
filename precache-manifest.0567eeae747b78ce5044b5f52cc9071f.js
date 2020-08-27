91c07a4f8ef43671229e39c5677b918c3d007634
Nishants878 <nishants878@gmail.com>
2020-08-27 14:35:06 +0530

    Updates

---
+++ precache-manifest.0567eeae747b78ce5044b5f52cc9071f.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9552bd13a35e9108438108ae4410363",
+    "url": "/Demo-Food-App%20/index.html"
+  },
+  {
+    "revision": "b9d9bd6f8051ae7bbf0b",
+    "url": "/Demo-Food-App%20/static/css/main.07543aa4.chunk.css"
+  },
+  {
+    "revision": "925712152775a4d1effd",
+    "url": "/Demo-Food-App%20/static/js/2.0f53ecc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Demo-Food-App%20/static/js/2.0f53ecc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d9bd6f8051ae7bbf0b",
+    "url": "/Demo-Food-App%20/static/js/main.ab7dcc7a.chunk.js"
+  },
+  {
+    "revision": "eae2fcc43a0c2a4867e4",
+    "url": "/Demo-Food-App%20/static/js/runtime-main.4d6e2371.js"
+  },
+  {
+    "revision": "68435d36327b5938fc94e09c4784971c",
+    "url": "/Demo-Food-App%20/static/media/Sandwich.68435d36.jpg"
+  },
+  {
+    "revision": "2d166038ad000d592bc034cf9eea7fd5",
+    "url": "/Demo-Food-App%20/static/media/bagel.2d166038.jpg"
+  },
+  {
+    "revision": "343332d61c47fceac664a74c6642beb2",
+    "url": "/Demo-Food-App%20/static/media/berger.343332d6.jpg"
+  },
+  {
+    "revision": "0677460ba4b26c0b5cbfbe0300d2ac5a",
+    "url": "/Demo-Food-App%20/static/media/delivery.0677460b.jpg"
+  },
+  {
+    "revision": "d8d87deb8b42fca8dd9c586809e8d2b3",
+    "url": "/Demo-Food-App%20/static/media/fries.d8d87deb.jpg"
+  },
+  {
+    "revision": "f1cde2e5ec239119c0a30ba20ed50be5",
+    "url": "/Demo-Food-App%20/static/media/img-all-food.f1cde2e5.png"
+  },
+  {
+    "revision": "c6b335b65ca418f215d856286a43017c",
+    "url": "/Demo-Food-App%20/static/media/img-arr-down.c6b335b6.png"
+  },
+  {
+    "revision": "e7db60efddad36de144745e3e694e822",
+    "url": "/Demo-Food-App%20/static/media/img-arr-right.e7db60ef.png"
+  },
+  {
+    "revision": "50e9c91d9170f84b3ba4c65600dfa6ec",
+    "url": "/Demo-Food-App%20/static/media/img-arrow-right.50e9c91d.png"
+  },
+  {
+    "revision": "2c51666dd501765c29992bbc9289de64",
+    "url": "/Demo-Food-App%20/static/media/img-asians.2c51666d.png"
+  },
+  {
+    "revision": "486a7c5578583ecf60df48faf23d0538",
+    "url": "/Demo-Food-App%20/static/media/img-barbeque.486a7c55.png"
+  },
+  {
+    "revision": "5e0c08cd23a99f3ff8681f00c9077d13",
+    "url": "/Demo-Food-App%20/static/media/img-burger.5e0c08cd.png"
+  },
+  {
+    "revision": "115224dc98b7168aa4e35c46d1aff204",
+    "url": "/Demo-Food-App%20/static/media/img-burgers.115224dc.png"
+  },
+  {
+    "revision": "01787237b5d25e26cdeea979e10f1c3f",
+    "url": "/Demo-Food-App%20/static/media/img-celeb.01787237.png"
+  },
+  {
+    "revision": "cb4ab16334cfcac6c18d4c18c0aef337",
+    "url": "/Demo-Food-App%20/static/media/img-clock.cb4ab163.png"
+  },
+  {
+    "revision": "671a65a8a5fc885b0b97c4c9c0b4d599",
+    "url": "/Demo-Food-App%20/static/media/img-dessert.671a65a8.png"
+  },
+  {
+    "revision": "97636784c3fd64dab3d22aade317ce82",
+    "url": "/Demo-Food-App%20/static/media/img-menu.97636784.png"
+  },
+  {
+    "revision": "410e248483e37367a500aeb0b3e25228",
+    "url": "/Demo-Food-App%20/static/media/img-pizza.410e2484.png"
+  },
+  {
+    "revision": "352b0441cbb82becdd2ef1e265b37e4f",
+    "url": "/Demo-Food-App%20/static/media/img-search.352b0441.png"
+  },
+  {
+    "revision": "d81a4aca5501ededa65411dac5a0ea3d",
+    "url": "/Demo-Food-App%20/static/media/img-smiley.d81a4aca.png"
+  },
+  {
+    "revision": "a4d24a7053eb6f30528ca48e7433d820",
+    "url": "/Demo-Food-App%20/static/media/img-star.a4d24a70.png"
+  },
+  {
+    "revision": "ec9a75df40f0908d8d742da8794731f8",
+    "url": "/Demo-Food-App%20/static/media/img-sushi.ec9a75df.png"
+  },
+  {
+    "revision": "671884223baaa215ef3f05efdf5ba71a",
+    "url": "/Demo-Food-App%20/static/media/img-thai.67188422.png"
+  },
+  {
+    "revision": "7afe97e679fa5e82ddcfe1f0fcdd48d0",
+    "url": "/Demo-Food-App%20/static/media/img-user.7afe97e6.png"
+  },
+  {
+    "revision": "e0d7608ecbdbd11bd0d7ebd83a2024df",
+    "url": "/Demo-Food-App%20/static/media/landing-illustration.e0d7608e.png"
+  },
+  {
+    "revision": "a7000140f206338bf43cc1b1a499500f",
+    "url": "/Demo-Food-App%20/static/media/pizza.a7000140.jpg"
+  },
+  {
+    "revision": "235ecbefe5e0d688a6de6ba536f84d7d",
+    "url": "/Demo-Food-App%20/static/media/sauce.235ecbef.jpg"
+  }
+]);
